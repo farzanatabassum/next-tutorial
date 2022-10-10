fd998a5afcf3d1a76f008c68325c048d9935430f
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-10 11:47:26 +0600

    Nav component & link

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import styles from '../styles/Layout.module.css'
+import Nav from './Nav'
 const Layout = ({children}) => {
   return (
-    <div className='styles.container'>
-        <main className="styles main">
+    <>
+    <Nav />
+    <div className={styles.container}>
+        <main className={styles.main}>
             {/* <h1>Hello</h1> */}
             {children}
             {/* passing root component as a children here */}
         </main>
       
     </div>
+    </>
   )
 }
 
